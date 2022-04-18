9262b071b07a75505d84ec64db8b77ce3b8ef0f0
DjelelBENABDELLAH <103830576+DjelelBENABDELLAH@users.noreply.github.com>
2022-04-18 18:41:43 +0200

    Update ExtractorSE.java

--- src/ExtractorSE.java
+++ src/ExtractorSE.java
@@ -1,14 +1,16 @@
 public class ExtractorSE extends Building {
     private int speed;
+    private Resource r;
 
-    public ExtractorSE(int s, String name) {
+    public ExtractorSE(int s, Resource r, String name) {
         super(name);
         speed = s;
+        this.r=r;
 
     }
 
     public String toString() {
-        return super.toString() + ", vitesse " + speed;
+        return super.toString() +",extrait la ressource "+r+ " à la vitesse " + speed ;
     }
 
 }
