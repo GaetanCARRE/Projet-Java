3079412b375db19672ecede103f17e14a37c3342
DjelelBENABDELLAH <103830576+DjelelBENABDELLAH@users.noreply.github.com>
2022-04-19 11:08:52 +0200

    Update Central.java

--- src/Central.java
+++ src/Central.java
@@ -2,16 +2,26 @@ public class Central extends CentralRE {
 
     private IsFuel fuel;
     private String category;
-    public Central(IsFuel fuel,String name,String t,int v, String c) throws DiffCategoryException
+    public Central(IsFuel fuel,String name,String t,int v, String c) throws DiffCategoryException, CategoryException
     {
         super(name,t,v);
         this.fuel = fuel;
         this.category = c;
+        if(!testCategory())
+            throw new CategoryException("la categorie de la centrale ne correspond pas à celle d'un carburant.");
         if (!fuel.getCategory().equals(c))
-            throw DiffCategoryException("La categorie de la centrale ne correspond pas à celle du carburant");
+            throw new DiffCategoryException("La categorie de la centrale ne correspond pas à celle du carburant.");
         
     }
 
+     public boolean testCategory()
+    {
+        if(category.equals("chemical") || category.equals("nuclear") || category.equals("antimatter"))
+        {
+            return true;                
+        }
+        return false;
+    }
     public int conso()
     {
         fuel.getValue()/value;
