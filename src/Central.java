06bce8d47b84c4e908e34c33ab4f025e7985a066
GaetanCARRE <gaetancarre.pro@gmail.com>
2022-04-19 10:15:26 +0200

    central

---
+++ src/Central.java
@@ -0,0 +1,20 @@
+public class Central extends CentralRE {
+
+    private IsFuel fuel;
+    private String category;
+    public Central(IsFuel fuel,String name,String t,int v)
+    {
+        super(name,t,v);
+        this.fuel = fuel;
+        this.category = fuel.getCategory();
+    }
+
+    public int conso()
+    {
+        fuel.getValue()/value;
+    }
+    public String toString()
+    {
+        return;
+    }
+}
