c9720fe6a2a7c727ab75535e3edfd43dd012fbc0
DjelelBENABDELLAH <103830576+DjelelBENABDELLAH@users.noreply.github.com>
2022-05-05 11:12:05 +0200

    Update Extractor.java

--- src/Extractor.java
+++ src/Extractor.java
@@ -29,10 +29,10 @@ public class Extractor extends ClassicFactory implements Extracteur{
     }
     public ArayList<Resource> getResource()
     {
-        if (tabr.size()>0)
+        //if (tabr.size()>0)
             return tabr;
-        else 
-            return null;
+        //else 
+        //    return null;
     }
 
 }
