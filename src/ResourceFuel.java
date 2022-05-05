b84a1badeb389afd09d0bcc8edc3a0d2d9dd4d21
DjelelBENABDELLAH <103830576+DjelelBENABDELLAH@users.noreply.github.com>
2022-05-05 11:18:38 +0200

    Update ResourceFuel.java

--- src/ResourceFuel.java
+++ src/ResourceFuel.java
@@ -2,6 +2,7 @@ public class ResourceFuel extends Resource implements IsFuel{
     
     private String category;
     private int value;
+    private ArrayList<Building> tabE;
 
     public ResourceFuel(String id, String name, /*Extractor e,*/ String c, int v) throws CategoryException
     {
@@ -22,7 +23,10 @@ public class ResourceFuel extends Resource implements IsFuel{
 
     public String toString()
     {
-        return super.toString() + ", categorie " +category + ", valeur " +value;
+        String s = "\n Resource minedby :\n";
+        for(Building b:tabE)
+            s += b;
+        return super.toString() + ", categorie " +category + ", valeur " +value + s;
     }
 
     public int getValue()
@@ -33,5 +37,14 @@ public class ResourceFuel extends Resource implements IsFuel{
     public String getCategory()
     {
         return category;
+    }
+    public void addE(Building E)
+    {
+        if((E instanceof ExtractorSE)||(E instanceof Building))
+            tabE.add(E);
+    }
+    public ArrayList<Building> getExtractor()
+    {
+        return tabE;
     }
 }
